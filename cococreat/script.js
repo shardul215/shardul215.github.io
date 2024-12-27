44d10b7503ec2e1126368f80bb4f184212a5be23
shardul215 <98656722+shardul215@users.noreply.github.com>
2024-12-27 21:13:18 +0530

    finial changes

--- cococreat/script.js
+++ cococreat/script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Hotel Paradise website loaded successfully!');
+    function toggleMenu() {
+        const nav = document.querySelector('nav');
+        nav.classList.toggle('open');
+    }
 });
+
+
